2085439e36f109154dfd6a74dfc40b2e6776688b
Tobias Steely <Tobias.steely@gmail.com>
2020-11-09 14:22:27 -0500

    Recursive Reflection

--- src/main/java/net/xilla/spigotcore/manager/MCObject.java
+++ src/main/java/net/xilla/spigotcore/manager/MCObject.java
@@ -9,6 +9,7 @@ import net.xilla.core.library.manager.XillaManager;
 import net.xilla.core.log.LogLevel;
 import net.xilla.core.log.Logger;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.serialization.ConfigurationSerializable;
 import org.bukkit.inventory.ItemStack;
@@ -32,30 +33,57 @@ public class MCObject extends ManagerObject {
     public XillaJson getSerializedData() {
         XillaJson json = new XillaJson();
 
-        Class<?> c = this.getClass();
-
         try {
-            for(Field field : getClass().getDeclaredFields()) {
-                if (field.getAnnotation(StoredData.class) != null) {
-                    Object object;
-                    if(!field.isAccessible()) {
-                        field.setAccessible(true);
+//            for(Field field : getClass().getDeclaredFields()) {
+//                if (field.getAnnotation(StoredData.class) != null) {
+//                    Object object;
+//                    if(!field.isAccessible()) {
+//                        field.setAccessible(true);
+//
+//                        object = field.get(this);
+//
+//                        field.setAccessible(false);
+//                    } else {
+//                        object = field.get(this);
+//                    }
+//
+//                    if(object instanceof SerializedObject) {
+//                        json.put(field.getName(), ((SerializedObject)object).getSerializedData().getJson());
+//                    } if(object instanceof ConfigurationSerializable) {
+//                        json.put(field.getName(), ((Location)object).serialize());
+//                    } else {
+//                        json.put(field.getName(), object);
+//                    }
+//                }
+//            }
 
-                        object = field.get(this);
+            Class<?> clazz = getClass();
+            while (clazz.getSuperclass() != null && !clazz.getSuperclass().getName().equals("MCObject")) {
+                for (Field field : clazz.getDeclaredFields()) {
+                    if (field.getAnnotation(StoredData.class) != null) {
+                        Object object;
+                        if (!field.isAccessible()) {
+                            field.setAccessible(true);
 
-                        field.setAccessible(false);
-                    } else {
-                        object = field.get(this);
-                    }
+                            object = field.get(this);
 
-                    if(object instanceof SerializedObject) {
-                        json.put(field.getName(), ((SerializedObject)object).getSerializedData().getJson());
-                    } if(object instanceof ConfigurationSerializable) {
-                        json.put(field.getName(), ((Location)object).serialize());
-                    } else {
-                        json.put(field.getName(), object);
+                            field.setAccessible(false);
+                        } else {
+                            object = field.get(this);
+                        }
+
+                        if (object instanceof SerializedObject) {
+                            json.put(field.getName(), ((SerializedObject) object).getSerializedData().getJson());
+                        } else if (object instanceof Material) {
+                            json.put(field.getName(), ((Material) object).name());
+                        } else if (object instanceof ConfigurationSerializable) {
+                            json.put(field.getName(), ((Location) object).serialize());
+                        } else {
+                            json.put(field.getName(), object);
+                        }
                     }
                 }
+                clazz = clazz.getSuperclass();
             }
             json.put("key", getKey());
             json.put("manager", getManager().getKey());
@@ -72,9 +100,50 @@ public class MCObject extends ManagerObject {
         if(json.containsKey("manager")) {
             setManager(XillaManager.getInstance().get(json.get("manager")));
         }
-        for(Field field : getClass().getDeclaredFields()) {
-            if (field.getAnnotation(StoredData.class) != null) {
-                if(json.containsKey(field.getName())) {
+//        for(Field field : getClass().getDeclaredFields()) {
+//            if (field.getAnnotation(StoredData.class) != null) {
+//                if(json.containsKey(field.getName())) {
+//                    try {
+//                        if(!field.isAccessible()) {
+//                            field.setAccessible(true);
+//
+//                            Object object = field.get(this);
+//
+//                            if(field.getType().isAssignableFrom(SerializedObject.class)) {
+//                                ((SerializedObject) object).loadSerializedData(new XillaJson(json.get(field.getName())));
+//                            } else if(field.getType().isAssignableFrom(ItemStack.class)) {
+//                                field.set(this, ItemStack.deserialize(json.get(field.getName())));
+//                            } else if(field.getType().isAssignableFrom(Location.class)) {
+//                                field.set(this, Location.deserialize(json.get(field.getName())));
+//                            } else {
+//                                field.set(this, json.get(field.getName()));
+//                            }
+//
+//                            field.setAccessible(false);
+//                        } else {
+//                            Object object = field.get(this);
+//
+//                            if(field.getType().isAssignableFrom(SerializedObject.class)) {
+//                                ((SerializedObject) object).loadSerializedData(new XillaJson(json.get(field.getName())));
+//                            } else if(field.getType().isAssignableFrom(ItemStack.class)) {
+//                                field.set(this, ItemStack.deserialize(json.get(field.getName())));
+//                            } else if(field.getType().isAssignableFrom(Location.class)) {
+//                                field.set(this, Location.deserialize(json.get(field.getName())));
+//                            } else {
+//                                field.set(this, json.get(field.getName()));
+//                            }
+//                        }
+//                    } catch (IllegalAccessException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }
+//        }
+
+        Class<?> clazz = getClass();
+        while (clazz.getSuperclass() != null && !clazz.getName().equals("MCObject")) {
+            for (Field field : clazz.getDeclaredFields()) {
+                if (field.getAnnotation(StoredData.class) != null) {
                     try {
                         if(!field.isAccessible()) {
                             field.setAccessible(true);
@@ -87,6 +156,8 @@ public class MCObject extends ManagerObject {
                                 field.set(this, ItemStack.deserialize(json.get(field.getName())));
                             } else if(field.getType().isAssignableFrom(Location.class)) {
                                 field.set(this, Location.deserialize(json.get(field.getName())));
+                            } else if(field.getType().isAssignableFrom(Material.class)) {
+                                field.set(this, Material.valueOf(json.get(field.getName())));
                             } else {
                                 field.set(this, json.get(field.getName()));
                             }
@@ -101,24 +172,18 @@ public class MCObject extends ManagerObject {
                                 field.set(this, ItemStack.deserialize(json.get(field.getName())));
                             } else if(field.getType().isAssignableFrom(Location.class)) {
                                 field.set(this, Location.deserialize(json.get(field.getName())));
+                            } else if(field.getType().isAssignableFrom(Material.class)) {
+                                field.set(this, Material.valueOf(json.get(field.getName())));
                             } else {
                                 field.set(this, json.get(field.getName()));
                             }
-//                            if(object instanceof SerializedObject) {
-//                                ((SerializedObject) object).loadSerializedData(new XillaJson(json.get(field.getName())));
-//                            } if(object instanceof ItemStack) {
-//                                field.set(this, ItemStack.deserialize(json.get(field.getName())));
-//                            } if(object instanceof Location) {
-//                                field.set(this, Location.deserialize(json.get(field.getName())));
-//                            } else {
-//                                field.set(this, json.get(field.getName()));
-//                            }
                         }
                     } catch (IllegalAccessException e) {
                         e.printStackTrace();
                     }
                 }
             }
+            clazz = clazz.getSuperclass();
         }
     }
 
